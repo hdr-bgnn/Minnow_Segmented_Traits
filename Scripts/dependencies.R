34fa944be99f6a87ac50722b4f588ef9f671c75a
Meghan Balk <meghan.balk@gmail.com>
2022-07-12 12:08:28 -0700

    Create dependencies.R

---
+++ Scripts/dependencies.R
@@ -0,0 +1,14 @@
+#file to install all the dependencies needed for the R code in /Scripts
+#Meghan A. Balk
+#balk@battelleecology.org
+
+##versions:
+#devtools 2.4.3
+#rjson 0.2.21
+#tidyr 1.2.0
+#dplyr 1.0.8
+#ggplot2 3.3.5
+#RColorBrewer 1.1.2
+#stringr 1.4.0
+
+install.packages(c("devtools", "rjson", "tidyr", "dplyr", "ggplot2", "RColorBrewer", "stingr"))
