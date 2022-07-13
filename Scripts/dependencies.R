24697c808d0f3e0c8ea077004975527a11fb61d8
Meghan Balk <meghan.balk@gmail.com>
2022-07-13 13:30:16 -0700

    Update dependencies.R

--- Scripts/dependencies.R
+++ Scripts/dependencies.R
@@ -1,22 +1,31 @@
-#file to install all the dependencies needed for the R code in /Scripts
-#Meghan A. Balk
-#balk@battelleecology.org
+################################################################################
+#' @title Installs dependencies for scripts in minnowTraits/Scripts. 
 
-##versions: (using packageVersion())
-#devtools 2.4.3
-#rjson 0.2.21
-#tidyr 1.2.0
-#dplyr 1.0.8
-#ggplot2 3.3.5
-#RColorBrewer 1.1.2
-#stringr 1.4.0
-#reshape2 1.4.4
+#' @author
+#' Meghan A. Balk \email{balk@battelleecology.org} \cr
 
-install.packages(c("devtools", 
-                   "rjson", 
-                   "tidyr", 
-                   "dplyr", 
-                   "ggplot2", 
-                   "RColorBrewer", 
-                   "stringr",
-                   "reshape2"))
+#' @description Installs dependencies needed for the .R files in 
+# minnowTraits/Scripts. The packages used in these scripts currently use the  
+# following versions: (version determined using packageVersion())
+# -devtools 2.4.3
+# -rjson 0.2.21
+# -tidyr 1.2.0
+# -dplyr 1.0.8
+# -ggplot2 3.3.5
+# -RColorBrewer 1.1.2
+# -stringr 1.4.0
+# -reshape2 1.4.4
+
+# changelog and author contributions
+#   Meghan Balk (2022-07-13)
+#     original creation
+################################################################################
+
+install.packages("devtools")
+
+devtools::install_version("rjson", version="0.2.21")
+devtools::install_version("tidyr", version="1.2.0")
+devtools::install_version("dplyr", version="1.0.8")
+devtools::install_version("ggplot2", version="3.3.5")
+devtools::install_version("RColorBrewer", version="1.1.2")
+devtools::install_version("stringr", version="1.4.0")
