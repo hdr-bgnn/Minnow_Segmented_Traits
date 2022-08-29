d82df832109f4c397be202e4d62c3f99f9640bf4
Meghan Balk <meghan.balk@gmail.com>
2022-08-29 09:50:17 -0700

    rearranging

--- Scripts/init.R
+++ Scripts/init.R
@@ -2,7 +2,13 @@
 # Meghan Balk 
 # balk@battelleecology.org
 
+#### load paths ----
+source(../paths)
+
 #### load functions ----
 source(file.path(scripts, "json_df.R"))
 source(file.path(scripts, "valid_url.R"))
 source(file.path(scripts, "download_size.R"))
+
+#### load manipulations ----
+source(file.path(scripts, "Data_Manipulation.R"))
--- paths.R
+++ paths.R
@@ -8,5 +8,6 @@ scripts <- "Scripts" # folder with scripts
 files <- "Files" # folder with files to read into scripts
 results <- "Results" # folder to store outputs of scripts
 figures <- file.path("Results", "Figures")
+workflow <- file.path("Workflow")
 presence <- file.path("Snakemake", "Morphology", "Presence") #this would be whatever snakemake produces. what folder would this be in?
 measure <- file.path("Snakemake", "Morphology", "Measure") #this would be whatever snakemake produces. what folder would this be in?
