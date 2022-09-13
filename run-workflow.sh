dc0329b64bb012e5245702ea38b5bea8502b8861
John Bradley <johnbradley2008@gmail.com>
2022-09-13 16:17:16 -0400

    Use Slurm to run the workflow
    Changes run-workflow.sh to configure Snakemake to
    launch sbatch jobs for the individual steps.
    Up to 20 jobs will be run at once.
    
    Removes the limit for just 10 files.

--- run-workflow.sh
+++ run-workflow.sh
@@ -5,7 +5,14 @@
 
 # Usage:
 # sbatch run-workflow.sh
+
+# Setup Snakemake/sbatch to use same account as this job
+export SBATCH_ACCOUNT=$SLURM_JOB_ACCOUNT
+
+# Configure Snakemake to run up to 20 jobs at once
+NUM_JOBS=20
+
 module load miniconda3
 source activate snakemake
 module load R/4.2.1-gnu11.2
-snakemake --cores 1 --use-singularity "$@"
+snakemake --jobs $NUM_JOBS --profile slurm/ --use-singularity "$@"
