d5b075620bc32fae0b36d1f530ce73785c73d784
John Bradley <johnbradley2008@gmail.com>
2023-02-04 07:31:09 -0500

    Add debug-workflow.sh
    Adds a script that will run the workflow in the foreground instead
    of background Slurm jobs. This is to simplify debugging problems
    with the workflow.

---
+++ debug-workflow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=PAS2136
+#SBATCH --job-name=MinnowTraits
+#SBATCH --time=03:00:00
+# Runs the Minnow Traits snakemake workflow 
+
+# Usage:
+# sbatch run-workflow.sh
+
+# Setup Snakemake/sbatch to use same account as this job
+export SBATCH_ACCOUNT=$SLURM_JOB_ACCOUNT
+
+# Configure Snakemake to run up to 20 jobs at once
+NUM_JOBS=20
+
+module load miniconda3
+source activate snakemake
+module load R/4.2.1-gnu11.2
+snakemake \
+    --jobs $NUM_JOBS \
+    --use-singularity \
+    --singularity-args "--bind $HOME/.dataverse" \
+    "$@"
